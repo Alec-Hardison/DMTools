b285754e540a8e183c87642313e9207bae878e08
Alec-Hardison <43781978+Alec-Hardison@users.noreply.github.com>
2018-10-16 09:02:20 -0400

    Add files via upload

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
 $(document).ready(function()
 {
- console.log("hello there world");   
+ $("input[type = 'radio']").click(function()
+    {
+        var radioValue = $("input[name = 'test']:checked").val();
+     console.log(radioValue);
+     if(radioValue)
+         {
+             console.log("we are finally working bob");
+         }
+    });
+    var imgArray = ["barovia.jpg","eberron.jpg","eragonMap.jpg","faerune.jpg"];
+   
     
-    
+    $("#map").append($("<img>").attr(
+        {
+            "src" : "barovia.jpg",
+            "height": "500",
+            "width" : "1000"
+        }));
+    $("#map").mousedown(function (e)
+        {
+            console.log(e.clientX);
+            console.log(e.clientY);
+        })
 });
